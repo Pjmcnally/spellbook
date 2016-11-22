7d7fcc84f9838231b2c2e75ee82497a80adbb9e7
Patrick McNally <Patrick.J.McNally@gmail.com>
2016-11-22 16:29:27 -0600

    begin to build data loader.

--- spellbook_proj/data_loader.py
+++ spellbook_proj/data_loader.py
@@ -1,16 +1,34 @@
 #!/usr/bin/env python
 import os
+import re
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "spellbook_proj.settings")
 import django  # noqa
 django.setup()
 
-from spellbook_app.models import CastingTime  # noqa
-from data_utils import get_file_list  # noqa
+from spellbook_app.models import (  # noqa
+    CastingTime, Duration, SpellRange, School, CharClass, Level,
+    Source, SpellSource, Component, SpellComponent, Spell)
+from data_utils import get_file_list   # noqa
+from django.utils.text import slugify  # noqa
 
 
-def parse(path):
-    print(path)
-    return path
+def get_or_create_casting_time(line):
+    cast_time_pat = re.compile(
+        '\*\*Casting Time\*\*:\s+'
+        '(?P<cast_time>[\d\w\s]+)'
+        '[,\s]*'
+        '(?P<react_text>[\d\w\s,]*)')
+
+    match = re.search(cast_time_pat, line)
+    cast_time = match.group("cast_time")
+    react_text = match.group("react_text")
+
+    obj, created = CastingTime.objects.get_or_create(text=cast_time)
+
+    return obj
+
+    # print("{a}{b}".format(
+    #     a=cast_time, b=", " + react_text if react_text else ""))
 
 
 def main():
@@ -18,8 +36,11 @@ def main():
     data_ext = ['.md']
 
     file_list = get_file_list(data_path, data_ext)
-    for file in file_list:
-        parse(file)
+    for file_path in file_list:
+        with open(file_path, 'r') as f:
+            content = [line.strip() for line in f if line.strip()]
+
+            get_or_create_casting_time(content[8])
 
 if __name__ == '__main__':
     main()
