0af1c59a8657c57b5d0d135dea140aab9d2d9022
Patrick McNally <Patrick.J.McNally@gmail.com>
2016-11-22 01:00:48 -0600

    add spellbook app to settings

--- spellbook_proj/spellbook_proj/settings.py
+++ spellbook_proj/spellbook_proj/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+from . import local_settings
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i08i1k$=^%#9@ng^s7bgpz(uaa)ewtaq1$t!a7h5qbd4j-%28p'
+SECRET_KEY = local_settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'spellbook_app',
 ]
 
 MIDDLEWARE = [
