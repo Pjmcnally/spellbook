66ae7fdc503a127264b554fd61dd362a339207aa
Patrick McNally <Patrick.J.McNally@gmail.com>
2016-11-21 20:09:28 -0600

    rename all .md files to end in .markdown

--- parse.py
+++ parse.py
@@ -1,21 +1,32 @@
-import os
+#!/usr/bin/env python
 # import re
+import os
 
 
 def get_files(path):
     files = []
+    bad_files = []
 
-    for file in os.walk(path):
-        if ".markdown" in file:
-            files.append(file)
+    for folder in os.walk(path):
+        for file in folder[2]:  # folder is a list.  The 2nd index lists files
+            if "markdown" in file:
+                files.append(file)
+            else:
+                bad_files.append(file)
 
-    return files
+    return files, bad_files
 
 
 def main():
     in_path = "./data"
     # out_path = "./output"
 
-    files = get_files(in_path)
+    files, bad_files = get_files(in_path)
 
-    print(files)
+    print("file count = {}".format(len(files)))
+    print("bad files count = {}".format(len(bad_files)))
+    for x in bad_files:
+        print(x)
+
+if __name__ == '__main__':
+    main()
