56b328053dab13aae6d90631ee7e99b929628ab4
Patrick McNally <Patrick.J.McNally@gmail.com>
2016-11-23 01:41:10 -0600

    Continue to parse raw data files to load into database

--- spellbook_proj/data_loader.py
+++ spellbook_proj/data_loader.py
@@ -6,30 +6,53 @@ import django  # noqa
 django.setup()
 
 from spellbook_app.models import (  # noqa
-    CastingTime, Duration, SpellRange, School, CharClass, Level,
-    Source, SpellSource, Component, SpellComponent, Spell)
+    CastingTime, Class, Component, Duration, Level, Range, School,
+    Source, SpellSource, Spell)
 from data_utils import get_file_list   # noqa
 from django.utils.text import slugify  # noqa
 
 
-def get_or_create_casting_time(line):
+def get_or_create_casting_time(string):
     cast_time_pat = re.compile(
         '\*\*Casting Time\*\*:\s+'
         '(?P<cast_time>[\d\w\s]+)'
         '[,\s]*'
         '(?P<react_text>[\d\w\s,]*)')
 
-    match = re.search(cast_time_pat, line)
+    match = re.search(cast_time_pat, string)
     cast_time = match.group("cast_time")
     react_text = match.group("react_text")
 
-    obj, created = CastingTime.objects.get_or_create(text=x)
+    obj, created = CastingTime.objects.get_or_create(
+        text=cast_time,
+        slug=slugify(cast_time))
 
     return obj, react_text
 
 
+def get_or_create_range(string):
+    range_pat = re.compile(
+        '^\*\*Range\*\*:\s+'
+        '(?P<_range>[\d\w\s]+)'
+        '[\s]*'
+        '(?P<range_text>[()\d\w\s\-]+)*$')
+
+    match = re.search(range_pat, string)
+    _range = match.group("_range").strip()
+    range_text = match.group("range_text")
+
+    if range_text:
+        range_text = range_text.replace("(", "").replace(")", "")
+
+    obj, created = Range.objects.get_or_create(
+        text=_range,
+        slug=slugify(_range))
+
+    return obj, range_text
+
+
 def main():
-    data_path = './data'
+    data_path = './data/raw_data'
     data_ext = ['.md']
 
     file_list = get_file_list(data_path, data_ext)
@@ -37,8 +60,9 @@ def main():
         with open(file_path, 'r') as f:
             content = [line.strip() for line in f if line.strip()]
 
-            cast_time = get_or_create_casting_time(content[8])
-            range_ = get_or_create_range(content[])
+            cast_time, react_text = get_or_create_casting_time(content[8])
+            # _class = get_or_create_class(content)
+            range_, react_text = get_or_create_range(content[9])
 
 if __name__ == '__main__':
     main()
