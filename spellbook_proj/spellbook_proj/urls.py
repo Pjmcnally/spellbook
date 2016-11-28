547c45e1c7610a19d97d112c9afb56abf9de2ded
Patrick McNally <Patrick.J.McNally@gmail.com>
2016-11-27 21:33:43 -0600

    add include to direct requests to app based urls

--- spellbook_proj/spellbook_proj/urls.py
+++ spellbook_proj/spellbook_proj/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
+from spellbook_app import urls as spellbook_urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^spellbook', include(spellbook_urls)),
 ]
