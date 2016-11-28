906598b4e6c9732cc58beab2815a798e85bee4b8
Patrick McNally <Patrick.J.McNally@gmail.com>
2016-11-27 21:34:05 -0600

    Add basic view to begin playing around

--- spellbook_proj/spellbook_app/views.py
+++ spellbook_proj/spellbook_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from .models import Spell, SpellSource
+
+
 # Create your views here.
+def display_spell(request, spell):
+    spell = Spell.objects.get(slug=spell)
+    context = {'spell': spell}
+    return render(request, 'spellbook_app/spell.html', context)
