389df71f50015fa4b098fec5a5421752ebde1807
Patrick McNally <Patrick.J.McNally@gmail.com>
2016-11-30 15:49:40 -0600

    Add url to direct to spell_list

--- spellbook_proj/spellbook_app/urls.py
+++ spellbook_proj/spellbook_app/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from .views import display_spell
+from .views import spell_detail, spell_list
 
 urlpatterns = [
-    url(r'spell/(?P<spell>[\w-]+)', display_spell, name='display_spell'),
+    url(r'^$', spell_list, name='spell_list'),
+    url(r'spell/(?P<spell>[\w-]+)/$', spell_detail, name='spell_detail'),
 ]
