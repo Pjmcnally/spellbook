93139c601954ae91af61c9a8ff2d699b47a545f9
Patrick McNally <Patrick.J.McNally@gmail.com>
2016-11-30 17:42:55 -0600

    add url for class spell lists

--- spellbook_proj/spellbook_app/urls.py
+++ spellbook_proj/spellbook_app/urls.py
@@ -4,5 +4,6 @@ from .views import spell_detail, spell_list
 
 urlpatterns = [
     url(r'^$', spell_list, name='spell_list'),
-    url(r'spell/(?P<spell>[\w-]+)/$', spell_detail, name='spell_detail'),
+    url(r'^class/(?P<slug>[\w-]+)/$', spell_list, name='class_spell_list'),
+    url(r'^spell/(?P<slug>[\w-]+)/$', spell_detail, name='spell_detail'),
 ]
