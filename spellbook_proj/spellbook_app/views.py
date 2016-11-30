9836091180ab28cc18691393aead57e990464272
Patrick McNally <Patrick.J.McNally@gmail.com>
2016-11-30 17:44:18 -0600

    add functionality to spell_list to all it to take a slug representing a class

--- spellbook_proj/spellbook_app/views.py
+++ spellbook_proj/spellbook_app/views.py
@@ -3,14 +3,22 @@ from django.shortcuts import render
 from .models import Class, Spell
 
 
-def spell_list(request):
-    classes = Class.objects.all()
-    context = {'classes': classes}
+def spell_list(request, slug=None):
+    classes = Class.objects.all()  # get all classes for navbar
+    if slug:
+        title_text = Class.objects.get(slug__iexact=slug).name.title()
+        spells = Spell.objects.filter(_class__slug=slug) \
+                              .filter(source__public=True)
+    else:
+        title_text = "All"
+        spells = Spell.objects.all().filter(source__public=True)
+
+    context = {'classes': classes, 'spells': spells, "title_text": title_text}
     return render(request, 'spellbook_app/spell_list.html', context)
 
 
-# Create your views here.
 def spell_detail(request, spell):
+    classes = Class.objects.all()  # get all classes for navbar
     spell = Spell.objects.get(slug=spell)
-    context = {'spell': spell}
+    context = {'classes': classes, 'spell': spell}
     return render(request, 'spellbook_app/spell_detail.html', context)
