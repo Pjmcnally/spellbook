f03b12badb1842768b4fd790662ab0a1fe2f8125
Patrick McNally <Patrick.J.McNally@gmail.com>
2016-11-30 15:50:05 -0600

    add function to render spell_list

--- spellbook_proj/spellbook_app/views.py
+++ spellbook_proj/spellbook_app/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 
-from .models import Spell, SpellSource
+from .models import Class, Spell
+
+
+def spell_list(request):
+    classes = Class.objects.all()
+    context = {'classes': classes}
+    return render(request, 'spellbook_app/spell_list.html', context)
 
 
 # Create your views here.
-def display_spell(request, spell):
+def spell_detail(request, spell):
     spell = Spell.objects.get(slug=spell)
     context = {'spell': spell}
-    return render(request, 'spellbook_app/spell.html', context)
+    return render(request, 'spellbook_app/spell_detail.html', context)
