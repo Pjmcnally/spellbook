572701bc860903f2f5b371f10be3f27b626e52ee
Patrick McNally <Patrick.J.McNally@gmail.com>
2016-12-01 01:18:06 -0600

    Break spells apart by level and add to context by levels

--- dnd/spellbook/views.py
+++ dnd/spellbook/views.py
@@ -7,13 +7,36 @@ def spell_list(request, slug=None):
     classes = Class.objects.all()  # get all classes for navbar
     if slug:
         title_text = Class.objects.get(slug__iexact=slug).name.title()
-        spells = Spell.objects.filter(_class__slug=slug) \
-                              .filter(source__public=True)
+        spells = Spell.objects.filter(_class__slug=slug)  # \
+                              # .filter(source__public=True)
     else:
         title_text = "All"
-        spells = Spell.objects.all().filter(source__public=True)
+        spells = Spell.objects.all()  # .filter(source__public=True)
 
-    context = {'classes': classes, 'spells': spells, "title_text": title_text}
+    cantrips = spells.filter(level__num=0)
+    spells_1 = spells.filter(level__num=1)
+    spells_2 = spells.filter(level__num=2)
+    spells_3 = spells.filter(level__num=3)
+    spells_4 = spells.filter(level__num=4)
+    spells_5 = spells.filter(level__num=5)
+    spells_6 = spells.filter(level__num=6)
+    spells_7 = spells.filter(level__num=7)
+    spells_8 = spells.filter(level__num=8)
+    spells_9 = spells.filter(level__num=9)
+
+    context = {
+        'classes': classes,
+        'title_text': title_text,
+        'cantrips': cantrips,
+        'spells_1': spells_1,
+        'spells_2': spells_2,
+        'spells_3': spells_3,
+        'spells_4': spells_4,
+        'spells_5': spells_5,
+        'spells_6': spells_6,
+        'spells_7': spells_7,
+        'spells_8': spells_8,
+        'spells_9': spells_9,}
     return render(request, 'spellbook/spell_list.html', context)
 
 
