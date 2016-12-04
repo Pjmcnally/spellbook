c2b437a90e342f00193d636ffe78aab0c3ed415f
Patrick McNally <Patrick.J.McNally@gmail.com>
2016-12-04 02:51:36 -0600

    move DATABASES settings to local settings for anonymity and to make it easier to switch in settings

--- dnd/dnd/settings.py
+++ dnd/dnd/settings.py
@@ -75,12 +75,7 @@ WSGI_APPLICATION = 'dnd.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = local_settings.DATABASES_sqlite
 
 
 # Password validation
