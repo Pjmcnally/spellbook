b6244ba005518befa8aab48c453975d7d52bef45
Patrick McNally <Patrick.J.McNally@gmail.com>
2016-12-10 21:56:46 -0600

    remove now obsolete URL parser

--- dnd/spellbook/urls.py
+++ dnd/spellbook/urls.py
@@ -5,6 +5,5 @@ from .views import spell_detail, spell_list, spell_content
 urlpatterns = [
     url(r'^$', spell_list, name='spell_list'),
     url(r'^spell_content$', spell_content, name='spell_content'),
-    url(r'^class/(?P<slug>[\w-]+)/$', spell_list, name='class_spell_list'),
     url(r'^spell/(?P<slug>[\w-]+)/$', spell_detail, name='spell_detail'),
 ]
