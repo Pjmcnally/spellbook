7aa79342d33efd94ad2113680cc1a699dc91988e
Patrick McNally <Patrick.J.McNally@gmail.com>
2016-12-11 21:59:27 -0600

    overhaul main.js to properly handly ajax loading of page

--- dnd/spellbook/static/spellbook/js/main.js
+++ dnd/spellbook/static/spellbook/js/main.js
@@ -1,69 +1,86 @@
 $(document).ready(main);
 
 function main() {
-    loadSpells();
-    setFakeLinkListener();
-
+    loadContent();
 }
 
+// Set listener to loadContent content on popstate event.
+$(window).bind("popstate", function() {
+    loadContent();
+});
+
+// Set listener to loadContent when nav link is clicked.
 $(function() {
     if (Modernizr.history) {
+        // history is support.  Use cool method.
 
+        // hijack the nav click event
+        $(".class-link").on("click", function(event) {
+            event.preventDefault();
+            _href = $(this).attr("href");
+
+            // change the url without a page refresh and add a history entry.
+            history.pushState(null, null, _href);
+
+            // load the content
+            loadContent();
+        });
     } else {
-
+        // History not supported.  Nothing Fancy here.
     }
 });
 
-window.onpopstate = function(event) {
-    console.log("navigated");
-};
-
-function setFakeLinkListener () {
-    $(".class-link").click(function(event) {
-        event.preventDefault();
-        loadSpells(event.target.id, "");
-        switchActive($(this));
-        // removeHash();
-    });
-}
-
-function switchActive (elem) {
-    $(".class-link").parent().removeClass("active");
-    $(elem).parent().addClass("active");
-}
-
-// function found at https://stackoverflow.com/questions/1397329/how-to-remove-the-hash-from-window-location-with-javascript-without-page-refresh/5298684#5298684
-function removeHash () {
-    history.pushState("", document.title,
-                      window.location.pathname + window.location.search);
-}
-
-function loadSpells (clss, search) {
-    urlPath = clss;
+// Load content from django database into page.
+function loadContent () {
+    _href = window.location.pathname;
+    clss = parseClassRef(_href);
     $.ajax({
         method: "post",
         url: "/spellbook/spell_content",
         data: {
             class: clss,
-            // search: search,
         },
         success: function(data){
             $("#content-box").html(data);
-            history.pushState({},"", urlPath);
+            switchActive(clss);
+            $('html,body').scrollTop(0);
         }
     });
 }
 
-function GetQueryStringParams(sParam) {
-    var queryString = window.location.search.substring(1);
-    var queryVars = queryString.split('&');
-    for (var i = 0; i < queryVars.length; i++) {
-        var param = queryVars[i].split('=');
-        if (param[0] == sParam) {
-            return param[1];
-        }
+
+// Parses class string from href.
+function parseClassRef (_href) {
+    var clssRe = /class\/([\w]+)/;
+    try {
+        clss = _href.match(clssRe)[1];
+        return clss;
+    } catch(err) {
+        return "";
     }
 }
+
+// Switches active higlighted nav button in main nav.
+function switchActive (clss) {
+    $(".class-link").parent().removeClass("active");
+    if (clss) {
+        clss_id = "#" + clss;
+    } else {
+        clss_id = "#all";
+    }
+    $(clss_id).parent().addClass("active");
+}
+
+// function GetQueryStringParams(sParam) {
+//     var queryString = window.location.search.substring(1);
+//     var queryVars = queryString.split('&');
+//     for (var i = 0; i < queryVars.length; i++) {
+//         var param = queryVars[i].split('=');
+//         if (param[0] == sParam) {
+//             return param[1];
+//         }
+//     }
+// }
 
 /* All functions belowed copied from Django documentation.
  * link = https://docs.djangoproject.com/en/1.10/ref/csrf/
