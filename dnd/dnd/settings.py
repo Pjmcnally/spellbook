809be82bec16017cd3309d31a0e79447c26b2d3a
Patrick McNally <Patrick.J.McNally@gmail.com>
2016-12-13 04:46:50 +0000

    moved local hosts to local settings

--- dnd/dnd/settings.py
+++ dnd/dnd/settings.py
@@ -26,11 +26,7 @@ SECRET_KEY = local_settings.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    'pjmcnally.pythonanywhere.com',
-    '127.0.0.1'
-]
-
+ALLOWED_HOSTS = local_settings.ALLOWED_HOSTS
 
 # Application definition
 
