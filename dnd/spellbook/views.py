e48b48aa90638c5e591897f2224073a7c829bf28
Patrick McNally <Patrick.J.McNally@gmail.com>
2016-12-13 06:51:14 +0000

    add ensure csfr decorater to ensure CSRF token

--- dnd/spellbook/views.py
+++ dnd/spellbook/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import ensure_csrf_cookie
 
 from .models import Clss, Spell
 
 
+@ensure_csrf_cookie
 def spell_list(request, slug=None):
     """ function to render spell list page """
     class_obj = None
